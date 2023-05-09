f2677ced1d90b8cc8cb03e00c5e5d12322003538
cmolinier <C@millE1706!>
2023-05-09 10:38:39 +0200

    debug score board + remove unused print

--- app/src/main/java/esir/progm/untitledsharkgames/menus/ScoreBoard.java
+++ app/src/main/java/esir/progm/untitledsharkgames/menus/ScoreBoard.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Explode;
 import android.transition.Fade;
+import android.util.Pair;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -15,12 +16,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.google.android.material.shape.ShapePath;
+
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 import esir.progm.untitledsharkgames.R;
+import esir.progm.untitledsharkgames.ScoreDB;
 
 public class ScoreBoard extends AppCompatActivity {
     private int hideSystemBars = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -29,6 +36,9 @@ public class ScoreBoard extends AppCompatActivity {
             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+    private InputStream is;
+    private FileOutputStream os;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,18 +101,19 @@ public class ScoreBoard extends AppCompatActivity {
     private HashMap<String, ArrayList<String>> loadScores(Context context){
         ArrayList<String> users = new ArrayList<>();
         ArrayList<String> scores = new ArrayList<>();
-        InputStream is = context.getResources().openRawResource(R.raw.scores);
-        BufferedReader r = new BufferedReader(new InputStreamReader(is));
-        String line;
+
+        is = getApplicationContext().getResources().openRawResource(R.raw.scores);
         try {
-            while ((line=r.readLine()) != null) {
-                System.out.println(line);
-                String[] splited = line.split(",");
-                users.add(splited[0]);
-                scores.add(splited[1]);
-            }
+            os = getApplicationContext().openFileOutput("scores.txt", Context.MODE_PRIVATE);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Ficher scores plus là !");
+        }
+
+        List<Pair<String, Integer>> leaderborad = ScoreDB.getInstance(is, os).getDB();
+
+        for(Pair<String, Integer> leader: leaderborad) {
+            users.add(leader.first);
+            scores.add(leader.second+"");
         }
         HashMap<String, ArrayList<String>> retMap = new HashMap<>();
         retMap.put("users", users);
