acee200b0867a73adb974e4b8f0464fabe00496d
cmolinier <C@millE1706!>
2023-05-14 14:37:55 +0200

    Clean UI disable in every class

--- app/src/main/java/esir/progm/untitledsharkgames/jeux/quiz/QuizActivity.java
+++ app/src/main/java/esir/progm/untitledsharkgames/jeux/quiz/QuizActivity.java
@@ -2,19 +2,32 @@ package esir.progm.untitledsharkgames.jeux.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentCallbacks2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import esir.progm.untitledsharkgames.MusicPlayer;
 import esir.progm.untitledsharkgames.R;
 import esir.progm.untitledsharkgames.ManageFiles;
 
 public class QuizActivity extends AppCompatActivity {
+    /*                    hide UI parameters                    */
+    private int hideSystemBars = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+    private boolean isOnBackground = false;
+
     private boolean isRight;
     private QuizQuestion qq;
 
@@ -28,9 +41,18 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_quiz);
+
+        View decor = getWindow().getDecorView();
+        decor.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if(visibility==0) {
+                decor.setSystemUiVisibility(hideSystemBars);
+            }
+        });
+
 
         this.intent = getIntent();
         Bundle b = this.intent.getExtras();
@@ -159,4 +181,35 @@ public class QuizActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isOnBackground) {
+            MusicPlayer.getInstance().pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!isOnBackground) {
+            MusicPlayer.getInstance().stop();
+            MusicPlayer.getInstance().play(getApplicationContext(), R.raw.main_menu, true);
+        } else {
+            MusicPlayer.getInstance().resume();
+            isOnBackground = false;
+        }
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        if(level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN ||
+                level == ComponentCallbacks2.TRIM_MEMORY_BACKGROUND ||
+                level == ComponentCallbacks2.TRIM_MEMORY_MODERATE ||
+                level == ComponentCallbacks2.TRIM_MEMORY_COMPLETE) {
+            isOnBackground = true;
+            onPause(); // https://i.kym-cdn.com/photos/images/original/000/639/420/094.gif
+        }
+    }
 }
--- app/src/main/java/esir/progm/untitledsharkgames/menus/QuizTrainingTheme.java
+++ app/src/main/java/esir/progm/untitledsharkgames/menus/QuizTrainingTheme.java
@@ -2,9 +2,11 @@ package esir.progm.untitledsharkgames.menus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentCallbacks2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,19 +14,37 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import esir.progm.untitledsharkgames.MusicPlayer;
 import esir.progm.untitledsharkgames.jeux.quiz.QuizActivity;
 import esir.progm.untitledsharkgames.R;
 import esir.progm.untitledsharkgames.ManageFiles;
 
 public class QuizTrainingTheme extends AppCompatActivity {
+    /*                    hide UI parameters                    */
+    private int hideSystemBars = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+    private boolean isOnBackground = false;
 
     private ArrayList<String> infos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_quiz_training_theme);
+
+        View decor = getWindow().getDecorView();
+        decor.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if(visibility==0) {
+                decor.setSystemUiVisibility(hideSystemBars);
+            }
+        });
 
         setButtons();
     }
@@ -90,5 +110,37 @@ public class QuizTrainingTheme extends AppCompatActivity {
         b.putString("theme", theme);
         intent.putExtras(b);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isOnBackground) {
+            MusicPlayer.getInstance().pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!isOnBackground) {
+            MusicPlayer.getInstance().stop();
+            MusicPlayer.getInstance().play(getApplicationContext(), R.raw.main_menu, true);
+        } else {
+            MusicPlayer.getInstance().resume();
+            isOnBackground = false;
+        }
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        if(level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN ||
+                level == ComponentCallbacks2.TRIM_MEMORY_BACKGROUND ||
+                level == ComponentCallbacks2.TRIM_MEMORY_MODERATE ||
+                level == ComponentCallbacks2.TRIM_MEMORY_COMPLETE) {
+            isOnBackground = true;
+            onPause(); // https://i.kym-cdn.com/photos/images/original/000/639/420/094.gif
+        }
     }
 }
