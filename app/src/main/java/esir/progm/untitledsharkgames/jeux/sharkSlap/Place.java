0ed30b718436576617593e059bd38a87f16b9d12
cmolinier <C@millE1706!>
2023-05-14 14:24:30 +0200

    clean code and comment 2

--- app/src/main/java/esir/progm/untitledsharkgames/jeux/sharkSlap/Place.java
+++ app/src/main/java/esir/progm/untitledsharkgames/jeux/sharkSlap/Place.java
@@ -1,31 +1,28 @@
 package esir.progm.untitledsharkgames.jeux.sharkSlap;
 
 import android.os.Handler;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 
 public class Place {
-
-    private ImageView imageView;
-    private String name;
-    private SharkSlap game;
+    /*                      attributes                       */
     private boolean isHidding;
     private boolean isHidden;
-
+    private ImageView imageView;
+    private SharkSlap game;
     private final Animation down_animation = new TranslateAnimation(0, 0, 0, 300);
     private final Animation up_animation = new TranslateAnimation(0, 0, 300, 0);
 
-    public Place(String name, ImageView imageView, SharkSlap game){
-
-        this.name = name;
+    public Place(ImageView imageView, SharkSlap game){
+        // Init parameters
         this.imageView = imageView;
         this.game = game;
         this.isHidding = false;
         isHidden = false;
 
+        // Set animations
         down_animation.setDuration(1000);
         down_animation.setFillAfter(true);
         up_animation.setDuration(500);
@@ -40,44 +37,31 @@ public class Place {
             public void onAnimationEnd(Animation arg0) {
                 isHidden = true;
                 isHidding = false;
-                imageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(!isHidding && !isHidden){
-                            game.addScore(100, false);
-                            down();
-                        }
+                imageView.setOnClickListener(v -> {
+                    if(!isHidding && !isHidden){
+                        game.addScore(100, false);
+                        down();
                     }
                 });
             }
         });
 
+        // Init the game
         init();
     }
 
     private void init() {
         down_animation.setDuration(1000);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                down();
-                imageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(!isHidding && !isHidden){
-                            game.addScore(100, false);
-                            down();
-                        }
-                    }
-                });
-            }
+        new Handler().postDelayed(() -> {
+            down();
+            imageView.setOnClickListener(v -> {
+                if(!isHidding && !isHidden){
+                    game.addScore(100, false);
+                    down();
+                }
+            });
         }, 1500);
-
-
-
     }
-
-    public boolean getIsHidden() {return  isHidden;}
 
     public void up() {
         if(isHidden) {
@@ -87,9 +71,7 @@ public class Place {
     }
 
     public void down() {
-        if(!isHidden) {
-            imageView.startAnimation(down_animation);
-        }
+        if(!isHidden) {imageView.startAnimation(down_animation);}
     }
 
 }
--- app/src/main/java/esir/progm/untitledsharkgames/jeux/sharkSlap/SharkSlap.java
+++ app/src/main/java/esir/progm/untitledsharkgames/jeux/sharkSlap/SharkSlap.java
@@ -1,14 +1,11 @@
 package esir.progm.untitledsharkgames.jeux.sharkSlap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ComponentCallbacks2;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +15,7 @@ import esir.progm.untitledsharkgames.R;
 import esir.progm.untitledsharkgames.interfaces.Game;
 
 public class SharkSlap extends Game {
+    /*                    hide UI parameters                    */
     private int hideSystemBars = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -26,25 +24,23 @@ public class SharkSlap extends Game {
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
     private boolean isOnBackground = false;
 
+    /*                    activity parameters                   */
     private int score;
     private TextView score_text;
     private ArrayList<Place> places;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Init activity and disable UI
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_shark_slap);
         View decor = getWindow().getDecorView();
-        decor.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener(){
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                if(visibility==0) {
-                    decor.setSystemUiVisibility(hideSystemBars);
-                }
-            }
+
+        decor.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if(visibility==0) {decor.setSystemUiVisibility(hideSystemBars);}
         });
 
         launch();
@@ -52,30 +48,28 @@ public class SharkSlap extends Game {
 
     @Override
     public void launch() {
+        // Set all sharks
         MusicPlayer.getInstance().stop();
-        places = new ArrayList<Place>();
-        places.add(new Place("shark1_1", findViewById(R.id.shark1_1), this));
-        places.add(new Place("shark1_2", findViewById(R.id.shark1_2), this));
-        places.add(new Place("shark1_3", findViewById(R.id.shark1_3), this));
-        places.add(new Place("shark2_1", findViewById(R.id.shark2_1), this));
-        places.add(new Place("shark2_2", findViewById(R.id.shark2_2), this));
-        places.add(new Place("shark2_3", findViewById(R.id.shark2_3), this));
-        places.add(new Place("shark3_1", findViewById(R.id.shark3_1), this));
-        places.add(new Place("shark3_2", findViewById(R.id.shark3_2), this));
-        places.add(new Place("shark3_3", findViewById(R.id.shark3_3), this));
+        places = new ArrayList<>();
+        places.add(new Place(findViewById(R.id.shark1_1), this));
+        places.add(new Place(findViewById(R.id.shark1_2), this));
+        places.add(new Place(findViewById(R.id.shark1_3), this));
+        places.add(new Place(findViewById(R.id.shark2_1), this));
+        places.add(new Place(findViewById(R.id.shark2_2), this));
+        places.add(new Place(findViewById(R.id.shark2_3), this));
+        places.add(new Place(findViewById(R.id.shark3_1), this));
+        places.add(new Place(findViewById(R.id.shark3_2), this));
+        places.add(new Place(findViewById(R.id.shark3_3), this));
 
+        // Init elements
         score = 0;
         score_text = findViewById(R.id.score_text);
         score_text.setText(score+"");
 
+        // Create new media player
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.shark_slap_init);
 
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                new SharkSlapGame(SharkSlap.this, getApplicationContext(), places).execute();
-            }
-        });
+        mediaPlayer.setOnCompletionListener(mediaPlayer1 -> new SharkSlapGame(SharkSlap.this, getApplicationContext(), places).execute());
 
         mediaPlayer.start();
     }
